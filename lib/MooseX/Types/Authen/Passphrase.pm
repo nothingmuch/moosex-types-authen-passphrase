8e10eed5ffb09132e633f8285dd4e83b1fe62f4f
Yuval Kogman <nothingmuch@woobling.org>
2009-07-18 14:46:02 -0700

    version bump

--- lib/MooseX/Types/Authen/Passphrase.pm
+++ lib/MooseX/Types/Authen/Passphrase.pm
@@ -5,7 +5,7 @@ package MooseX::Types::Authen::Passphrase;
 use strict;
 use warnings;
 
-our $VERSION = "0.01";
+our $VERSION = "0.02";
 
 use Authen::Passphrase;
 use Authen::Passphrase::RejectAll;
